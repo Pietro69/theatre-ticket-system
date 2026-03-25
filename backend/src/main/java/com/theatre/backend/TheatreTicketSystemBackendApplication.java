51fc00179423d1924466f959f823e572f3faaa7b
Sekvojak <dominikkontrik123@gmail.com>
2026-03-25 19:47:34 +0100

    fix: wrong time of created plays v2

--- backend/src/main/java/com/theatre/backend/TheatreTicketSystemBackendApplication.java
+++ backend/src/main/java/com/theatre/backend/TheatreTicketSystemBackendApplication.java
@@ -1,9 +1,12 @@
 package com.theatre.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
@@ -14,4 +17,8 @@ public class TheatreTicketSystemBackendApplication {
 		SpringApplication.run(TheatreTicketSystemBackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Bratislava"));
+	}
 }
